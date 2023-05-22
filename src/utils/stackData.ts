9f46ddb040cf916d312cbd1e2a225528d31ed7fc
luisfernandomrs <luisfernandomrs@outlook.com>
2023-05-22 15:59:06 -0400

    fix imports

--- src/utils/stackData.ts
+++ src/utils/stackData.ts
@@ -3,30 +3,30 @@ import { SiJavascript, SiHtml5, SiCss3 } from "react-icons/si";
 export const stackData = [
   {
     title: "HTML",
-    img: "src/assets/imagens/html5.svg",
+    img: "src/public/static/img/imgTask/html5.svg",
   },
   {
     title: "CSS",
-    img: "src/assets/imagens/css.svg",
+    img: "src/public/static/img/imgTask/css.svg",
   },
   {
     title: "JS",
-    img: "src/assets/imagens/javascript.svg",
+    img: "src/public/static/img/imgTask/javascript.svg",
     // img: "src/assents/imagens/javascript.svg",
   },
-  { title: "TypeScript", img: "src/assets/imagens/typescript.svg" },
+  { title: "TypeScript", img: "src/public/static/img/imgTask/typescript.svg" },
   {
     title: "Node JS",
-    img: "src/assets/imagens/nodejs.svg",
+    img: "src/public/static/img/imgTask/nodejs.svg",
   },
   {
     title: "React",
-    img: "src/assets/imagens/reagir.svg",
+    img: "src/public/static/img/imgTask/reagir.svg",
   },
-  { title: "Jdango", img: "src/assets/imagens/icons8-django.svg" },
+  { title: "Jdango", img: "src/public/static/img/imgTask/icons8-django.svg" },
   // { title: "Next.js", img: TbBrandNextjs },
   // { title: "Docker", img: FaDocker },
-  { title: "PostgreSQL", img: "src/assets/imagens/postgreesql.svg" },
-  { title: "Git", img: "src/assets/imagens/git.svg" },
-  { title: "Python", img: "src/assets/imagens/python.svg" },
+  { title: "PostgreSQL", img: "src/public/static/img/imgTask/postgreesql.svg" },
+  { title: "Git", img: "src/public/static/img/imgTask/git.svg" },
+  { title: "Python", img: "src/public/static/img/imgTask/python.svg" },
 ];
