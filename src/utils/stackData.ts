69c7ccae24ab6445059b68d2d9ea9fac196497ff
luisfernandomrs <luisfernandomrs@outlook.com>
2023-05-22 17:52:51 -0400

    fix imports stacks icons

--- src/utils/stackData.ts
+++ src/utils/stackData.ts
@@ -1,32 +1,42 @@
 import { SiJavascript, SiHtml5, SiCss3 } from "react-icons/si";
+import css from "@/public/static/img/imgTask/css.svg";
+import html from "@/public/static/img/imgTask/html5.svg";
+import git from "@/public/static/img/imgTask/git.svg";
+import django from "@/public/static/img/imgTask/icons8-django.svg";
+import react from "@/public/static/img/imgTask/reagir.svg";
+import javascript from "@/public/static/img/imgTask/javascript.svg";
+import typescript from "@/public/static/img/imgTask/typescript.svg";
+import nodejs from "@/public/static/img/imgTask/nodejs.svg";
+import python from "@/public/static/img/imgTask/python.svg";
+import postgreesql from "@/public/static/img/imgTask/postgreesql.svg";
 
 export const stackData = [
   {
     title: "HTML",
-    img: "src/public/static/img/imgTask/html5.svg",
+    img: html,
   },
   {
     title: "CSS",
-    img: "src/public/static/img/imgTask/css.svg",
+    img: css,
   },
   {
     title: "JS",
-    img: "src/public/static/img/imgTask/javascript.svg",
+    img: javascript,
     // img: "src/assents/imagens/javascript.svg",
   },
-  { title: "TypeScript", img: "src/public/static/img/imgTask/typescript.svg" },
+  { title: "TypeScript", img: typescript },
   {
     title: "Node JS",
-    img: "src/public/static/img/imgTask/nodejs.svg",
+    img: nodejs,
   },
   {
     title: "React",
-    img: "src/public/static/img/imgTask/reagir.svg",
+    img: react,
   },
-  { title: "Jdango", img: "src/public/static/img/imgTask/icons8-django.svg" },
+  { title: "Jdango", img: django },
   // { title: "Next.js", img: TbBrandNextjs },
   // { title: "Docker", img: FaDocker },
-  { title: "PostgreSQL", img: "src/public/static/img/imgTask/postgreesql.svg" },
-  { title: "Git", img: "src/public/static/img/imgTask/git.svg" },
-  { title: "Python", img: "src/public/static/img/imgTask/python.svg" },
+  { title: "PostgreSQL", img: postgreesql },
+  { title: "Git", img: git },
+  { title: "Python", img: python },
 ];
